ee2085a5291ce6116622dcf791e9ce7be96a9240
Jorel Huerto <jjhue29@gmail.com>
2025-09-26 13:56:04 -0700

    refamiliarizing with rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::{env, process};
+
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    
+    let command = Command::new(&args).unwrap_or_else(|err|{
+        eprintln!("Error parsing arguements {err}");
+        process::exit(1);
+    });
+}
+
+struct Command {
+    a: f32,
+    b: f32,
+    op: String,
+}
+
+impl Command {
+    fn new(args: &[String]) -> Result<Command, &'static str>{
+        
+        let a:Result<f32, std::num::ParseFloatError> = args[1].parse::<f32>();
+
+        let b:Result<f32, std::num::ParseFloatError> = args[2].parse::<f32>();
+        
+        if b.is_err() || a.is_err(){
+            return Err("Cannot parse for number arguement");
+        }
+
+        let op: String = args[3].clone();
+        
+        Ok(Command {a:a.unwrap(), b:b.unwrap(), op})
+   }
 }
